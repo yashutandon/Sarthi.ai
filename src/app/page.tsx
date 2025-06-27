c031a7eb269fbc86fb12eb8fc9da109948601765
yashu tandon <yashutandon56@gmail.com>
2025-06-28 04:08:27 +0530

    authenticated the github or  google social and protect non auth pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,17 @@
-'use client'
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import { authClient } from "@/lib/auth-client";
+import { auth } from "@/lib/auth";
+import { HomeView } from "@/modules/home/ui/views/home-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
+const Page = async() => {
+  const session = await auth.api.getSession({
+    headers:await headers(),
+  })
 
-export default function Home() {
-
-  const { data: session, } = authClient.useSession() 
-
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-
-  const onSubmit =()=>{
-    authClient.signUp.email({
-      email,
-      password,
-      name
-    },{
-      onError:()=>{
-        window.alert("something went wrong")
-      },
-      onSuccess:()=>{
-        window.alert(" success")
-      }
-    })
-
+  if(!session){
+    redirect('/sign-in')
   }
-    const onLogin =()=>{
-    authClient.signIn.email({
-      email,
-      password,
-    },{
-      onError:()=>{
-        window.alert("something went wrong")
-      },
-      onSuccess:()=>{
-        window.alert(" success")
-      }
-    })
-
-  }
-
-  if(session){
-    return(
-      <div className="flex flex-col p-4 gap-y-4">
-        <p>Logged in as {session.user.name}</p>
-        <Button onClick={()=>authClient.signOut()}>Sign Out</Button>
-      </div>
-    )
-  }
-  
-  return (
-   <div>
-     <div className="p-4 flex flex-col gap-y-4">
-      <Input placeholder="name" value={name} onChange={(e)=>setName(e.target.value)}/>
-      <Input placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-      <Input placeholder="password" type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-
-      <Button onClick={onSubmit}>
-        Create user
-      </Button>
-    </div>
-     <div className="p-4 flex flex-col gap-y-4">
-      <Input placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-      <Input placeholder="password" type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-
-      <Button onClick={onLogin}>
-        Login
-      </Button>
-    </div>
-   </div>
-  )
+  return <HomeView/>;
 }
+ 
+export default Page;
--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -1,159 +1,195 @@
-'use client'
-import { authClient } from '@/lib/auth-client'
-import { z } from 'zod'
-import {zodResolver} from '@hookform/resolvers/zod'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { Form,FormControl,FormField,FormItem,FormLabel,FormMessage } from '@/components/ui/form'
-import { Alert,AlertTitle } from '@/components/ui/alert'
-import { OctagonAlert } from 'lucide-react'
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {FaGithub,FaGoogle} from 'react-icons/fa'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { OctagonAlert } from "lucide-react";
 
-import { Card,CardContent } from '@/components/ui/card'
-import Image from 'next/image'
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 
 const formSchema = z.object({
-  email:z.string().email(),
-  password:z.string().min(1,{message:"Password is required"}),
+  email: z.string().email(),
+  password: z.string().min(1, { message: "Password is required" }),
 });
 
-
 export const SignInView = () => {
-  const router =useRouter();
-  const [pending,setPending]=useState(false);
-  const [error,setError]=useState<string| null>(null);
+ 
+  const router=useRouter();
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState<string | null>(null);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues:{
-      email:"",
-      password:""
-    }
-  })
-  const onSubmit= (data:z.infer<typeof formSchema>)=>{
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
     setError(null);
     setPending(true);
-     authClient.signIn.email({
-      email:data.email,
-      password:data.password
-    },{
-      onSuccess:()=>{
-        setPending(false)
-        router.push("/")
+    authClient.signIn.email(
+      {
+        email: data.email,
+        password: data.password,
+        callbackURL:'/'
       },
-      onError:({error})=>{
-        setPending(false)
-        setError(error.message);
+      {
+        onSuccess: () => {
+          setPending(false);
+          router.push('/')
+        },
+        onError: ({ error }) => {
+          setPending(false);
+          setError(error.message);
+        },
       }
-    }
-  );
+    );
+  };
 
+  const onSocial = (provider: "github" | "google") => {
+    setError(null);
+    setPending(true);
+    authClient.signIn.social(
+      {
+        provider: provider,
+        callbackURL:'/'
+      },
+      {
+        onSuccess: () => {
+          setPending(false);
+          
+        },
+        onError: ({ error }) => {
+          setPending(false);
+          setError(error.message);
+        },
+      }
+    );
   };
 
   return (
-    <div className='flex flex-col gap-6'>
-      <Card className='overflow-hidden p-0'>
-        <CardContent className='grid p-0 md:grid-cols-2'>
+    <div className="flex flex-col gap-6">
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}  className='p-6 md:p-8'>
-              <div className='flex flex-col gap-6'>
-                <div className='flex flex-col items-center text-center'>
-                    <h1 className='text-2xl font-bold'>
-                      Welcome back
-                    </h1>
-                    <p className='text-muted-foreground text-balance'>
-                      Login to your account
-                    </p>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col items-center text-center">
+                  <h1 className="text-2xl font-bold">Welcome back</h1>
+                  <p className="text-muted-foreground text-balance">
+                    Login to your account
+                  </p>
                 </div>
-                <div className='grid gap-3'>
+                <div className="grid gap-3">
                   <FormField
-                  control={form.control}
-                  name='email'
-                  render={({field})=>(
-                    <FormItem>
-                      <FormLabel>
-                        Email
-                      </FormLabel>
-                       <FormControl>
-                        <Input
-                        type='email'
-                        placeholder='Enter your email address' 
-                        {...field}
-                        />
-                       </FormControl>
-                       <FormMessage />
-                    </FormItem>
-                  )}
-                  
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="Enter your email address"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
                 </div>
-                                <div className='grid gap-3'>
+                <div className="grid gap-3">
                   <FormField
-                  control={form.control}
-                  name='password'
-                  render={({field})=>(
-                    <FormItem>
-                      <FormLabel>
-                        Password
-                      </FormLabel>
-                       <FormControl>
-                        <Input
-                        type='Password'
-                        placeholder='Enter your  Password' 
-                        {...field}
-                        />
-                       </FormControl>
-                       <FormMessage />
-                    </FormItem>
-                  )}
-                  
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="Password"
+                            placeholder="Enter your  Password"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
                 </div>
                 {!!error && (
-                  <Alert className='bg-destructive/10 border-none'>
-                  <OctagonAlert className='h-4 w-4 !text-destructive'/>
-                  <AlertTitle>{error}</AlertTitle>
-                </Alert>)}
-                <Button disabled={pending} type='submit' className='w-full '>Sign In</Button> 
-                <div className='after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t'>
-                  <span className='bg-card text-muted-foreground relative z-10 px-2'>
+                  <Alert className="bg-destructive/10 border-none">
+                    <OctagonAlert className="h-4 w-4 !text-destructive" />
+                    <AlertTitle>{error}</AlertTitle>
+                  </Alert>
+                )}
+                <Button disabled={pending} type="submit" className="w-full ">
+                  Sign In
+                </Button>
+                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                  <span className="bg-card text-muted-foreground relative z-10 px-2">
                     Or continue with
                   </span>
                 </div>
-                <div className='grid grid-cols-3'>
-                  <Button disabled={pending} variant='outline' type='button' className='w-full'>
-                    Google
+                <div className="grid grid-cols-2">
+                  <Button
+                    onClick={() => onSocial("google")}
+                    disabled={pending}
+                    variant="outline"
+                    type="button"
+                    className="w-full"
+                  >
+                    <FaGoogle/>
                   </Button>
-                  <Button disabled={pending} variant='outline' type='button' className='w-full'>
-                    Github
+                  <Button
+                    onClick={() => onSocial("github")}
+                    disabled={pending}
+                    variant="outline"
+                    type="button"
+                    className="w-full"
+                  >
+                    <FaGithub/>
                   </Button>
-                  <Button disabled={pending} variant='outline' type='button' className='w-full'>
-                    Apple
-                  </Button>
-                </div>  
-                <div className='text-center text-sm'>
-                  Don&apos;t have an account?{" "} 
-                  <Link href="/sign-up" className='underline underline-offset-4'>Sign Up</Link>
+                </div>
+                <div className="text-center text-sm">
+                  Don&apos;t have an account?{" "}
+                  <Link
+                    href="/sign-up"
+                    className="underline underline-offset-4"
+                  >
+                    Sign Up
+                  </Link>
                 </div>
               </div>
             </form>
           </Form>
-          <div className='bg-radial from-blue-100 to-gray-400 relative hidden md:flex flex-col gap-y-4 items-center justify-center'>
-            <Image 
-              src="/logo.svg" 
-              alt="Logo" 
-              height={1900} 
-              width={1900} 
-            />
-            
+          <div className="bg-radial from-blue-100 to-gray-400 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+            <Image src="/logo.svg" alt="Logo" height={1900} width={1900} />
           </div>
         </CardContent>
       </Card>
-      <div className='text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4'>
-                  By clicking continue, you agree to our {" "} <a href="#">Terms of Service </a> and <a href="#">Privacy Policy</a>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="#">Terms of Service </a>{" "}
+        and <a href="#">Privacy Policy</a>
       </div>
     </div>
-  )
-}
+  );
+};
