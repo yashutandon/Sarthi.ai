677a99ffc6fa5e95ed65e45f0a0c027cc697264a
yashu tandon <yashutandon56@gmail.com>
2025-07-01 00:25:58 +0530

    implement the protectedProcedure,agentCreate, list header , new agent dialog and agent form

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -4,12 +4,27 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import {ErrorBoundary} from 'react-error-boundary'
 
 import React, { Suspense } from "react";
+import ListHeader from "@/modules/agents/ui/views/components/agents-list-header";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
+
+  const session = await auth.api.getSession({
+      headers:await headers(),
+    })
+  
+    if(!session){
+      redirect('/sign-in')
+    }
+
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
 
   return (
+    <>
+    <ListHeader/>
     <HydrationBoundary state={dehydrate(queryClient)}>
       <Suspense fallback={<AgentsViewLoading/>}>
       <ErrorBoundary fallback={<AgentsViewError/>}>
@@ -17,6 +32,7 @@ const Page = async () => {
       </ErrorBoundary>
       </Suspense>
     </HydrationBoundary>
+    </>
   );
 };
 
---
+++ src/modules/agents/ui/views/components/agents-list-header.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { PlusIcon } from 'lucide-react'
+import React, { useState } from 'react'
+import NewAgentDialog from './new-agent-dialog'
+
+const ListHeader = () => {
+    const [isDialogOpen,setIsDialogOpen]=useState(false);
+  return (
+    <>
+    <NewAgentDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}/>
+    <div className='py-4 px-4 md:px-8 flex flex-col gap-y-4'>
+        <div className='flex items-center justify-between'>
+            <h5 className='font-medium text-xl'>My Agents</h5>
+            <Button onClick={()=>setIsDialogOpen(true)}>
+                <PlusIcon/>
+                New Agent
+            
+            </Button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default ListHeader
