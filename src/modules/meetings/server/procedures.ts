fd1a1bdb5cd406e7f2e4a62d299fe4627bfa863b
yashu tandon <yashutandon56@gmail.com>
2025-07-08 16:06:21 +0530

    Connecting the agents in the meeting(video call) with the help openai

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -89,7 +89,7 @@ export const meetingsRouter = createTRPCRouter({
             },
             settings_override:{
               transcription:{
-                language:"hi",
+                language:"en",
                 mode:'auto-on',
                 closed_caption_mode:"auto-on",
               },
